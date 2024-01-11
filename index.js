78fad7189d4a6e8285e94808c068994a2c6b4a5c
Tomás Aravena <taravenab@uft.edu>
2024-01-11 16:35:50 -0300

    SA-LPD2- Ejercicio de lógica de programación - Práctica de conversión de temperaturas

---
+++ index.js
@@ -0,0 +1,5 @@
+let temperatura_celsius = parseFloat(prompt("¿Cuantos grados celsius hay hoy?"))
+let temperatura_farenheit = (temperatura_celsius * 9/5) + 32
+let temperatura_kelvin = temperatura_celsius + 273.15
+
+document.write(`Hay ${temperatura_celsius} grados celsius, lo que equivale a ${temperatura_farenheit} grados Farenheit  y ${temperatura_kelvin} grados Kelvin`)
