d81d8597f71b91a798006721facfbea88f8f30a9
Tomás Aravena <taravenab@uft.edu>
2024-01-11 17:31:15 -0300

    este si

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-let temperatura_celsius = parseFloat(prompt("¿Cuantos grados celsius hay hoy?"))
+let temperatura_celsius;
 
-if (isNaN(temperatura_celsius)) {
+
+do {
+  let input = prompt("¿Cuántos grados Celsius hay hoy?");
+  temperatura_celsius = parseFloat(input);
+
+  
+  if (isNaN(temperatura_celsius)) {
     alert("Por favor, ingrese un número válido.");
   }
+} while (isNaN(temperatura_celsius));
 
-let temperatura_farenheit = (temperatura_celsius * 9/5) + 32
-let temperatura_kelvin = temperatura_celsius + 273.15
+let temperatura_farenheit = (temperatura_celsius * 9/5) + 32;
+let temperatura_kelvin = temperatura_celsius + 273.15;
 
-document.write(`Hay ${temperatura_celsius} grados celsius, lo que equivale a ${temperatura_farenheit} grados Farenheit  y ${temperatura_kelvin} grados Kelvin`)
+document.write(`Hay ${temperatura_celsius} grados Celsius, lo que equivale a ${temperatura_farenheit} grados Fahrenheit y ${temperatura_kelvin} grados Kelvin`);
